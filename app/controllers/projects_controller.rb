9872a50c8e68bbccf22eb17a809f1a2de20cef7d
mariangela-l <mariangela.aum@gmail.com>
2016-11-16 00:14:58 +0000

    authentication and some project functionality

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,68 @@
+class ProjectsController < ApplicationController
+	before_action :authenticate_user!, except: [:index, :show]
+	before_action :set_project, only: [:show, :edit, :update, :destroy]
+
+
+	def index
+		@projects = Project.all
+		#@displayed.projects = Project.take(4)
+	end
+
+	def show
+
+	end
+
+	def new
+		@project = Project.new
+	end
+
+	def edit
+	end
+
+	def create
+		@project = current_user.projects.build(project_params)
+
+		respond_to do |format|
+			if @project.save
+				format.html { redirect_to @project, notice: "Project was succesfully created."}
+				format.json { render :show, status: :ok, location: @project }
+			else
+				format.html { render :new }
+				format.json { render json: @project.errors, status: :unprocessable_entity }
+			end
+		end
+	end
+
+	def update
+		respond_to do |format|
+			if @project.update(project_params)
+				format.html { redirect_to @project, notice: "Project was succesfully updated."}
+				format.json { render :show, status: :ok, location: @project }
+			else
+				format.html { render :edit }
+				format.json { render json: @project.errors, status: :unprocessable_entity }
+			end
+		end
+	end
+
+	def destroy
+		@project.destroy
+		respond_to do |format|
+			format.html { redirect_to root_path, notice: "Project was succesfully removed."}
+			format.json { head :no_content }
+		end
+	end
+
+private
+
+
+	def set_project
+		@project = Project.find(params[:id])
+	end
+
+	def project_params
+		params.require(:project).permit(:title, :summary, :description, :goal, :image_url, :expiration_date, :category_id)
+	end
+end
+
+
---
+++ db/migrate/20161115160736_create_projects.rb
@@ -0,0 +1,19 @@
+class CreateProjects < ActiveRecord::Migration
+	def change
+		create_table :projects do |t|
+			t.references :user, index: true, foreign_key: true
+			t.string  :title
+			t.text :summary
+			t.text :description
+			t.string :image_url
+			t.references :location, index: true
+			t.references :category, index: true
+			t.references :skill, index: true
+			t.decimal :goal, precision: 8, scale: 2
+			t.datetime :expiration_date
+			t.string :status, default: "pending"
+
+			t.timestamps null: false
+		end
+  end
+end
